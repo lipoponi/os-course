f056df012ae9bb91753e5c940e8304ad1c375eba
Tebloev Stanislav <stas.tebloev@yandex.com>
2019-09-22 16:04:56 +0300

    Apply new output pattern

--- lab3/task1.sh
+++ lab3/task1.sh
@@ -2,5 +2,5 @@
 
 echo -n "Count:"
 ps axo user= | grep -c ^user
-ps axo user=,pid=,cmd= | awk '/^user/ {print $2 ":" $3}' > user.txt
-echo "List stored in ./user.txt"
+ps axo user=,pid=,cmd= | awk '/^user/ {print $2 ":" $3}' > task1.out
+echo "List stored in ./task1.out"
--- lab3/task3.sh
+++ lab3/task3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ps axo pid=,cmd= | awk '$2 ~ /^\/sbin\// {print $1}' > sbin.txt
-echo "Output stored in ./sbin.txt"
+ps axo pid=,cmd= | awk '$2 ~ /^\/sbin\// {print $1}' > task3.out
+echo "Output stored in ./task3.out"
