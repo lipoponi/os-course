0def9712b6e55651c476b8e9294ca58f7533cae1
Tebloev Stanislav <stas.tebloev@yandex.com>
2019-09-22 13:59:54 +0300

    Modified lab3/task1.sh

--- lab3/task1.sh
+++ lab3/task1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo -n "Count: "
-ps au | grep -c ^user
-ps au | awk '/^user/ {printf "%s:%s\n", $2, $11}' > user.txt
+ps_output="$(ps axu)"
+echo "$ps_output" | grep -c ^user
+echo "$ps_output" | awk '/^user/ {printf "%s:%s\n", $2, $11}' > user.txt
 echo "List stored at ./user.txt"
