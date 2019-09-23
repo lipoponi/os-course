0455b5813f99a7647f69a50075ee03843eb9e860
Stanislav Tebloev <stas.tebloev@yandex.com>
2019-09-23 21:20:46 +0300

    Update task2.sh

--- lab1/task2.sh
+++ lab1/task2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [[ $1 -lt $2 ]]
+if [[ "$1" -lt "$2" ]]
 then
-  if [[ $2 -lt $3 ]]
-  then result=$3
-  else result=$2
+  if [[ "$2" -lt "$3" ]]
+  then result="$3"
+  else result="$2"
   fi
 else
-  if [[ $1 -lt $3 ]]
-  then result=$3
-  else result=$1
+  if [[ "$1" -lt "$3" ]]
+  then result="$3"
+  else result="$1"
   fi
 fi
 
